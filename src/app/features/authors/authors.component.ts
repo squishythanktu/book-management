485cf4b52109dd3157a6764c8702d88a23f324a9
Le Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2023-03-08 14:01:20 +0700

    reconfig .scss file

--- src/app/features/authors/authors.component.ts
+++ src/app/features/authors/authors.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-authors',
   templateUrl: './authors.component.html',
-  styleUrls: ['./authors.component.css']
+  styleUrls: ['./authors.component.scss']
 })
 export class AuthorsComponent {
 
--- src/app/features/books/book-list/book-item/book-item.component.ts
+++ src/app/features/books/book-list/book-item/book-item.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-book-item',
   templateUrl: './book-item.component.html',
-  styleUrls: ['./book-item.component.css']
+  styleUrls: ['./book-item.component.scss']
 })
 export class BookItemComponent {
 
