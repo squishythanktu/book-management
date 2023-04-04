0a051f06ea40abd385eb3de2f945df953200c59c
Le Thanh Tu (YooMie) <87054146+squishythanktu@users.noreply.github.com>
2023-04-04 08:17:52 +0700

    merge 3e93371

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
-import { BookGuard } from './core/guards/book.guard';
+import { SignUpGuard } from './core/guards/signup.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AuthComponent } from './features/auth/auth.component';
 import { SignupComponent } from './features/signup/signup.component';
+import { BookGuard } from './core/guards/book.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/books', pathMatch: 'full' },
